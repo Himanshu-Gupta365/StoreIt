57e4b32d36ce0512c3cf9b33eab7dea64e608de8
Himanshu Gupta <himanshucse49@gmail.com>
2025-12-16 20:31:46 +0530

    First commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex-center h-screen">
+      <h1 className="text-3xl font-bold text-brand "> StoreIt - The only storage solution you need.</h1>
+    </div>
+  );
+}
